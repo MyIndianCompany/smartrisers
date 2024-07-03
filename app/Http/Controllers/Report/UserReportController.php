f923ae0b3faa62da306c39c76026dd2aedafa578
meiteiamarkanta <meiteiamarkantanaorem@gmail.com>
2024-07-03 11:52:07 +0530

    user report add validation

--- app/Http/Controllers/Report/UserReportController.php
+++ app/Http/Controllers/Report/UserReportController.php
@@ -40,31 +40,25 @@ class UserReportController extends Controller
 
     public function store(Request $request)
     {
-        // Validate the incoming request
         $request->validate([
             'reported_user_id' => 'exists:users,id',
-//            'report_description' => 'required',
-            'files.*' => 'file|mimes:jpg,jpeg,png,pdf,doc,docx,txt|max:2048' // Adjust mime types and size limit as needed
+            'report_description' => 'required',
+            'files.*' => 'required|file|mimes:jpg,jpeg,png,pdf,doc,docx,txt|max:2048'
         ]);
 
         try {
             DB::beginTransaction();
             $reporter = auth()->user()->id;
-            // Create the user report
             $userReport = UserReport::create([
                 'reporter_id' => $reporter,
                 'reported_user_id' => $request->reported_user_id,
                 'report_description' => $request->report_description
             ]);
-
-            // Handle file uploads
             if ($request->hasFile('files')) {
                 foreach ($request->file('files') as $file) {
-                    // Store the file
                     $filePath = $file->getRealPath();
                     $uploadResult = CloudinaryLabs::upload($filePath)->getSecurePath();
                     $url = $uploadResult;
-                    // Save the file details in UserReportFile model
                     UserReportFile::create([
                         'user_report_id' => $userReport->id,
                         'original_file_name' => $file->getClientOriginalName(),
@@ -74,7 +68,6 @@ class UserReportController extends Controller
                 }
             }
             DB::commit();
-            // Return a response
             return response()->json(['message' => 'Report created successfully'], 201);
         } catch (\Exception $exception) {
             DB::rollBack();
